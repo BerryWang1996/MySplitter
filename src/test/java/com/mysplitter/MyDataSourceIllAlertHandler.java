e00303e816dfa4738ab6d24980e1565b7b1bf966
berry <951332197@qq.com>
2018-05-23 21:42:07 +0800

    数据源异常警告添加获取异常的字段

--- src/test/java/com/mysplitter/MyDataSourceIllAlertHandler.java
+++ src/test/java/com/mysplitter/MyDataSourceIllAlertHandler.java
@@ -1,6 +1,7 @@
 package com.mysplitter;
 
 import com.mysplitter.advise.MySplitterDataSourceIllAlerterAdvise;
+import org.slf4j.LoggerFactory;
 
 /**
  * @Author: wangbor
@@ -8,9 +9,10 @@ import com.mysplitter.advise.MySplitterDataSourceIllAlerterAdvise;
  */
 public class MyDataSourceIllAlertHandler implements MySplitterDataSourceIllAlerterAdvise {
 
+    private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(MyDataSourceIllAlertHandler.class);
+
     @Override
-    public void illAlerter(String databaseName, String nodeName) {
-
+    public void illAlerter(String databaseName, String nodeName, Exception e) {
+        LOGGER.error("database:{}, node:{}", databaseName, nodeName, e);
     }
-
 }
